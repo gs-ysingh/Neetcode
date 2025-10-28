6bfc6ef149a88b5d28d6805a239f53ef9a6f95e9
Yogesh Singh <yogeshsingh@microsoft.com>
2025-10-28 21:31:09 +0530

    Use minHeap function

--- Heap/problem 215.js
+++ Heap/problem 215.js
@@ -1,14 +1,8 @@
 var findKthLargest = function(nums, k) {
-    this.minHeap = new MinPriorityQueue();
-    for(let i = 0; i < k; i++) {
-        this.minHeap.enqueue(nums[i]);
-    }
-    for(let i = k; i < nums.length; i++) {
-        if(nums[i] > this.minHeap.front().element) {
-            this.minHeap.dequeue();
-            this.minHeap.enqueue(nums[i]);
-        }
-    }
-
-    return this.minHeap.front().element;
+  const minHeap = new MinHeap();
+  for (const num of nums) {
+    minHeap.push(num);
+    if (minHeap.size() > k) minHeap.pop();
+  }
+  return minHeap.top();
 };
